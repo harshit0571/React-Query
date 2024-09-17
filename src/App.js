83db8485a9149826bff93c4da68e7cac9009176d
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-09-17 12:09:41 +0530

    basic react query structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { wait } from "@testing-library/user-event/dist/utils";
+
+const Posts = [
+  { id: 1, title: "post 1" },
+  { id: 2, title: "post 2" },
+];
 
 function App() {
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...Posts]),
+  });
+
+  if (postQuery.isLoading) return <h1>Loading.....</h1>;
+  if (postQuery.isError) return <pre>{JSON.stringify(postQuery.error)}</pre>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
